ac54bc09e75365f922cc78f0205cf4fd62afe108
Philipp Schneider <schneider.philipp@uni-muenster.de>
2020-02-25 22:32:57 +0100

    Added lemma extraction
    Preprocessing now includes creation of a file with stored lemmas

--- scripts/preprocess-historia-hierosylitama.py
+++ scripts/preprocess-historia-hierosylitama.py
@@ -11,6 +11,7 @@ import sys
 import csv
 import time
 import datetime
+import ast
 
 # Get current timestamp for provenance-date
 timestamp = time.time()
@@ -20,23 +21,45 @@ timestamp = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:
 with open("data/Historia_Hierosylitama/Historia Hierosolymitana_with_lemmas.xml") as historia_file:
     historia_tei = historia_file.read()
 
+# Create new file to store preprocessed text
 new_text_file = open("data/Historia_Hierosylitama/01 Segmentation/historia-hierosylitama.txt", "w")
 historia_writer = csv.writer(new_text_file, delimiter = "\t")
+
+# Creta new file to store lemmas
+lemma_file = open("data/Historia_Hierosylitama/01 Segmentation/historia-hierosylitama.lemma", "w")
 
 h_soup = BeautifulSoup(historia_tei, "xml")
 
 sentences = h_soup.find_all("s")
 #prepared_text = ""
 line_index = 1200000001
+lemmas = ""
 
-print("Extracting text from TEI file...")
+print("Extracting text and lemmas from TEI file...")
 
 for s in sentences:
-    #prepared_text += s.get_text() + "\n"
+    # Extract and store text from TEI
     new_line = s.get_text().encode("utf8")
     historia_writer.writerow([line_index] + [new_line] + [timestamp] + ["Historia-Hierosylitama"])
     line_index += 1
 
+    # Extract and store lemmas from TEI
+    words = s.find_all("w")
+    
+    for w in words:
+        current_word = w.get_text()
+        new_lemma = w.get("lemma")
+
+        if new_lemma is not None and current_word not in lemmas:
+            new_lemma = ast.literal_eval(w.get("lemma"))        # cast string to list and dict
+            new_lemma = new_lemma[0]["name"].split("@")
+            lemmas += current_word + "\t" + new_lemma[0] + "\t" + new_lemma[1] + "\n"
+
+# Write lemmas to file
+lemmas = lemmas.encode("utf8")
+lemma_file.write(lemmas)
+
 new_text_file.close()
+lemma_file.close()
 
 print("Done")
