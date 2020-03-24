4a7e71b4504691b95b44092ce1dfea367d6d7274
EC2 Default User <ec2-user@ip-172-31-38-154.eu-central-1.compute.internal>
2020-03-24 10:04:55 +0000

    Tracer analysis vulgata francorum

--- tracer/data/corpora/1_2_vulgata_francorum/TRACER_DATA/01-02-WLP-lem_true_syn_true_ssim_false_redwo_false-ngram_5-LLR_true_toLC_true_rDia_false_w2wl_false-wlt_5/01-01-01-00-00-WordBasedTrainingImpl/02-02-01-01-01-LocalMaxFeatureFrequencySelectorImpl_FeatDens=0.8/02-01-01-01-01-1_2_vulgata_francorum-01-01-01-02-1_2_vulgata_francorum/02-02-01-01-01-02-SelectedFeatureResemblanceSimilarityImpl_Threshold=0.9/001-TraVizPostprocessingImpl-mode=1_minfrequency=2/texts.js
+++ tracer/data/corpora/1_2_vulgata_francorum/TRACER_DATA/01-02-WLP-lem_true_syn_true_ssim_false_redwo_false-ngram_5-LLR_true_toLC_true_rDia_false_w2wl_false-wlt_5/01-01-01-00-00-WordBasedTrainingImpl/02-02-01-01-01-LocalMaxFeatureFrequencySelectorImpl_FeatDens=0.8/02-01-01-01-01-1_2_vulgata_francorum-01-01-01-02-1_2_vulgata_francorum/02-02-01-01-01-02-SelectedFeatureResemblanceSimilarityImpl_Threshold=0.9/001-TraVizPostprocessingImpl-mode=1_minfrequency=2/texts.js
@@ -1,1 +1,1 @@
-var texts = [{title:"vulgata",min:1100000001,max:1100031086},{title:"vulgata1200000001",min:1100031087,max:1100031087},{title:"historia-francorum",min:1200000002,max:1200001382}];
+var texts = [{title:"vulgata",min:1100000001,max:1100031087},{title:"historia-francorum",min:1200000001,max:1200001382}];
