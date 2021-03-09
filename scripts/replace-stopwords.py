25949a69cad7a850e096b98783286566eb23a730
Philipp Schneider <schneider.philipp@uni-muenster.de>
2021-03-09 18:02:05 +0100

    Cleanup comments

--- scripts/replace-stopwords.py
+++ scripts/replace-stopwords.py
@@ -1,13 +1,18 @@
 # replace-stopwords.py
-# @author: Philipp Schneider
-# @date: 2020-04-10
-#
-# Removes all stopwords listet in the file latin.txt from the vulgate. Currently no input parameters
+"""
+@author: Philipp Schneider
+@date: 2020-04-10
+
+Removes all stopwords listed in the file latin.txt from the vulgate. Currently no input parameters -- input file is
+set via the global variable file_path.
+"""
+
+file_path = "tracer-master/data/corpora/18_apocalypsis_francorum_no-stopwords/18_apocalypsis_francorum_no-stopwords.txt"
 
 with open("tracer-master/data/stopwords/latin.txt", "r") as stopword_file:
     stopwords = stopword_file.readlines()
 
-with open("tracer-master/data/corpora/1_5_vulgata_francorum/1_3_vulgata_francorum.txt", "r") as corpus_file:
+with open(file_path, "r") as corpus_file:
     corpus = corpus_file.read()
 
 for stopword_entry in stopwords:
@@ -15,6 +20,9 @@ for stopword_entry in stopwords:
     stopword_entry = stopword_entry.replace("\n", "")
     stopword = " " + stopword_entry + " "
     corpus = corpus.replace(stopword, " ")
+
+    stopword = "\t" + stopword_entry + " "
+    corpus = corpus.replace(stopword, "\t")
 
     stopword = " " + stopword_entry + "."
     corpus = corpus.replace(stopword, " ")
@@ -27,6 +35,6 @@ for stopword_entry in stopwords:
 
 print(corpus)
 
-corpus_file = open("tracer-master/data/corpora/1_5_vulgata_francorum/1_3_vulgata_francorum.txt", "w")
+corpus_file = open(file_path, "w")
 corpus_file.write(corpus)
 corpus_file.close()
